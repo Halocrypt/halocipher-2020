cd58eb9226025a2892a44483ae5a74dbd3ac4d0f
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-04-27 17:13:13 +0530

    Added Random Sorting ( @NullExceptions are still there )

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 """
     Halocipher: Copyright 2020 (Utkarsh Dubey, Dhruv Bhatia)
-
     Halocipher was built for Halocrypt 2020, you can always use this commercially if you want to get hacked.
-
 """
 
 # Imports
 from functools import partial
 from io import StringIO
-import random
+import random, json
 
 
 """
@@ -27,20 +25,19 @@ class File():
 
 """
     Encrypt/Decrypt Functions
-
 """
-def encrypt(message: str):
+def encrypt(message):
 
     """
     Converting the text into a List() of chunks with the chunk_size = 10
     """
 
     # Lowercasing the message content
-    block_string = message.lower()
+    block_string = u'{}'.format(message.lower())
 
     # Chunk Code
     chunk_size = 10
-    chunks = [l for l in iter(partial(StringIO(message).read, chunk_size), '')]
+    chunks = [l for l in iter(partial(StringIO(block_string).read, chunk_size), '')]
     
     """
     Converting chunks into Blocks :-->
@@ -70,17 +67,47 @@ def encrypt(message: str):
 
         block["signature"] = signature_count
         block["chunk"] = chunk_dict
-        block["encrypted"] = random.sample(list(chunk_dict.fromkeys(range(100))), 10)
+        
+        """
+            Sorting: Randomly picking letters for encrypted/schema file.
+            <Eg: _great --> (_gat),(re)
+        """
+        
+         # Encrypted and Schema Blocks
+        chunk_random_indices = random.sample(range(0, len(chunk)), random.randint(0, len(chunk)))
+
+        if not chunk_random_indices:
+            chunk_random_indices.append(0)
+
+        if len(chunk_random_indices) == len(chunk):
+            chunk_random_indices = random.sample(range(0, len(chunk)), random.randint(0, len(chunk)))
+
+        chunk_encrypted = {}
+        chunk_schema = {}
+
+
+        for indice in chunk_dict:
+            if int(indice) in chunk_random_indices:
+                chunk_encrypted[str(indice)] = chunk_dict.get(indice)
+            else:
+                chunk_schema[str(indice)] = chunk_dict.get(indice)
+
+        if not chunk_schema:
+            chunk_schema["0"] = chunk_dict.get("0")
+
+        if not chunk_encrypted:
+            chunk_encrypted["0"] = chunk_dict.get("0")
+
+        block["encrypted"] = chunk_encrypted
+        block["schema"] = chunk_schema
+
+
 
         blocks.append(block)
         signature_count += signature_increment
 
-    """
-    Sorting: Randomly picking letters for encrypted/schema file.
-    <Eg: _great --> (_gat),(re)
 
-    """
         
     return blocks
 
-print(encrypt("Twenty One Pilots is great"))
+print(json.dumps(encrypt("Twenty One Pilots is great"), indent=4))
